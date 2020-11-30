24903b993f2e4829b3a5927c0b42eee26014fa5c
Kristian Rekstad <krirek@msn.com>
2020-11-30 21:48:43 +0100

    feat: Use good logging settings

--- start_modelserver.sh
+++ start_modelserver.sh
@@ -3,10 +3,20 @@
 PORT=48212
 FIX_JAVA_REFLECTION="--add-opens java.base/java.lang=ALL-UNNAMED"
 
+# Uses log4j version 1.
+LOG_SETTINGS=-Dlog4j.configuration="file:lib/log4j.xml"
+LOG4J_DEBUG=-Dlog4j.debug
+
 # Requires java 11 or above!
 # Starting it will read all files in the workspace, as it looks for EResources.
-java $FIX_JAVA_REFLECTION -jar lib/emf_modelserver_standalone.jar --port $PORT --root examples
+java $FIX_JAVA_REFLECTION $LOG_SETTINGS -jar lib/emf_modelserver_standalone.jar --port $PORT --root examples
 
 # Test with http://localhost:48212/api/v1/models
 # or http://localhost:48212/api/v1/server/ping
 # Docs: https://github.com/eclipse-emfcloud/emfcloud-modelserver
+
+# A curl request to create a new model file:
+# curl --location --request POST 'http://localhost:48212/api/v1/models?modeluri=MyTest.ecore&format=json' \
+#--header 'Content-Type: application/json' \
+#--data-raw '{"data":"{\"eClass\":\"http://www.eclipse.org/emf/2002/Ecore#//EPackage\",\"name\":\"MyPackage\",\"nsURI\":\"platform:/TreeLanguageServerProtocol/src/MyModel.ecore\",\"nsPrefix\":\"no.krex.test.ecore\"}"}
+#'
